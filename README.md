883c2bda570739f97e988f28fe77b130fe921a65
Marc Gritsch <marc.gritsch@gmail.com>
2016-02-05 15:39:54 +0100

    add FilterableProductTable from Facebook Tutorial

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 # Tutorial
 https://www.twilio.com/blog/2015/08/setting-up-react-for-es6-with-webpack-and-babel-2.html
+https://facebook.github.io/react/docs/thinking-in-react.html
 
 # init project
 npm init
--- main.js
+++ main.js
@@ -2,3 +2,4 @@ import Hello from './jsx/hello.jsx';
 import World from './jsx/world.jsx';
 import Test from './jsx/test.jsx';
 import Button from './jsx/button.jsx';
+import FilterableProductTable from './jsx/filterableProductTable.jsx';
